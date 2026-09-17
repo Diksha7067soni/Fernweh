f1681f7305c5b8b046c48524ffd633642ab34bec
Diksha soni <dikshasoni7067@email.com>
2026-09-17 18:46:24 +0530

    changes undo

--- init/index.js
+++ init/index.js
@@ -1,30 +1,55 @@
 const mongoose = require("mongoose");
+
 const initData = require("./data.js");
+
 const Listing = require("../models/listing.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/Fernweh";
-const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/Fernweh";
+
+
 
 main()
-    .then(async() => {
-        console.log("connected to db");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+
+.then(async() => {
+
+    console.log("connected to db");
+
+      
+})
+
+  .catch((err) => {
+
+         console.log(err);
+
+       });
 
 async function main() {
-    await mongoose.connect(dbUrl);
+
+      
+    await mongoose.connect(MONGO_URL);
+
 }
 
 const initDB = async() => {
+
+      
     await Listing.deleteMany({});
+
+      
     initData.data = initData.data.map((obj) => ({
-        ...obj,
-        owner: "6aa39bc87ac1ad7710069a6f"
-    }));
+
+            ...obj,
+
+            owner: "6aa39bc87ac1ad7710069a6f"
+
+           }));
+
+      
     await Listing.insertMany(initData.data);
+
+      
     console.log("data was initialized");
+
 };
 
 initDB();
