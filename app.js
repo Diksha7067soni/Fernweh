4d5e43fde5d7b5585b6f6e8747f8b8ca44b4861c
Diksha soni <dikshasoni7067@email.com>
2026-09-17 18:32:03 +0530

    fixing mapbox token

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 console.log("MAP TOKEN EXISTS:", !!process.env.MAP_TOKEN);
-console.log("MAP TOKEN START:", process.env.MAP_TOKEN ? .substring(0, 3));
-
+console.log("MAP TOKEN START:", process.env.MAP_TOKEN && process.env.MAP_TOKEN.substring(0, 3));
 
 const dns = require("dns");
 
